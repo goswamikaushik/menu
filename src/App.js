5ab064cd3c13d2fd16201170ae70de0065d32355
goswamikaushik <goswamikaushik2003@gmail.com>
2024-03-29 15:50:54 +0530

    starting with resuable button component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Button from "./components/Button";
+
+const App = () => {
+  return (
+    <div>
+      <Button>Primary</Button>
+      <Button>Secondary</Button>
+      <Button>Warning</Button>
+      <Button>Success</Button>
+      <Button>Danger</Button>
+    </div>
+  );
+};
+
+export default App;
