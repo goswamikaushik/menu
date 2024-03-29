870a7cf3b1783326237edd40636d1123da3514b9
goswamikaushik <goswamikaushik2003@gmail.com>
2024-03-29 18:28:11 +0530

    starting with tailwind css

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import Button from "./components/Button";
 const App = () => {
   return (
     <div>
-      <Button>Primary</Button>
-      <Button>Secondary</Button>
-      <Button>Warning</Button>
-      <Button>Success</Button>
-      <Button>Danger</Button>
+      <div>
+        <Button primary>Primary</Button>
+      </div>
+      <div>
+        <Button>Secondary</Button>
+      </div>
+      <div>
+        <Button>Warning</Button>
+      </div>
+      <div>
+        <Button>Success</Button>
+      </div>
+      <div>
+        <Button>Danger</Button>
+      </div>
     </div>
   );
 };
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,34 @@
 import React from "react";
 
-const Button = ({ children }) => {
-  return (
-    <div>
-      <button>{children}</button>
-    </div>
-  );
+const Button = ({
+  children,
+  primary,
+  danger,
+  secondary,
+  warning,
+  success,
+  outline,
+  rounded,
+}) => {
+  return <button>{children}</button>;
+};
+
+Button.propTypes = {
+  checkVariationValue: ({ primary, danger, secondary, warning, success }) => {
+    console.log(primary);
+    const count =
+      Number(!!primary) +
+      Number(!!secondary) +
+      Number(!!warning) +
+      Number(!!success) +
+      Number(!!danger);
+
+    if (count > 1) {
+      return new Error(
+        "Only one of primary, secondary, success, wanring, danger can be true"
+      );
+    }
+  },
 };
 
 export default Button;
