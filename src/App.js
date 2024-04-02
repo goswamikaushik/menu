ec10fd03a2b76974dd7b2070f5bbd18b201d2fa8
goswamikaushik <goswamikaushik2003@gmail.com>
2024-04-02 17:06:17 +0530

    add navigation and put all the thing together

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import Route from "./components/Route";
+import AccordionPage from "./pages/AccordionPage";
+import DropdownPage from "./pages/DropdownPage";
+import ButtonPage from "./pages/ButtonPage";
+import SideBar from "./components/SideBar";
+
 const App = () => {
-  return <div>asdasda</div>;
+  return (
+    <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
+      <SideBar />
+      <div className="col-span-5">
+        <Route path="/accordion">
+          <AccordionPage />
+        </Route>
+        <Route path="/">
+          <DropdownPage />
+        </Route>
+        <Route path="/button">
+          <ButtonPage />
+        </Route>
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/hooks/use-navigation.js
@@ -0,0 +1,9 @@
+import { useContext } from "react";
+
+import NavigationContext from "../context/navigation";
+
+const useNavigation = () => {
+  return useContext(NavigationContext);
+};
+
+export default useNavigation;
